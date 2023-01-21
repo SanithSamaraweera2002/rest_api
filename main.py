61e3e04cbe8159eb4d356e6c338718cffff362c7
Sanith Samaraweera <sanithsamaraweera2002@gmail.com>
2023-01-21 13:27:14 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from db import db
 
+
 from resource.hello_get import HelloGet
 from resource.hello_post import HelloPost
+
 
 application = Flask(__name__)
 
 api = Api(application)
 
+#application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['database_url']
 application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin123@localhost:5432/rest_api_tutorial'
 
 db.init_app(application)
